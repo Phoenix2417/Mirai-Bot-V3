221fcb0ea1de0ae6be6128f7bcf0e9449380a888
HoangDev <154896136+Yggdrasil07@users.noreply.github.com>
2024-10-06 10:48:49 +0700

    Update setqtv.js

--- modules/commands/setqtv.js
+++ modules/commands/setqtv.js
@@ -1,107 +1,105 @@
-module.exports.config = {
-    name: "setqtv",
-    version: "1.0.0",
-    hasPermssion: 1,
-    credits: "ErikaOwO",
-    description: "Set quản trị viên cho người khác!",
-    commandCategory: "Admin",
-    usages: ".../add @tag | phản hồi/remove @tag | phản hồi",
-    cooldowns: 5
-};
-module.exports.run = async function ({ event, api, Currencies, args ,Users, Threads }) {
-  //if(!args[0]) return api.sendMessage('co cai db', event.threadID)
-  let dataThread = (await Threads.getData(event.threadID)).threadInfo;
-  if (args.length == 0) return api.sendMessage(`=== 『 SET QUẢN TRỊ VIÊN 』 ===\n━━━━━━━━━━━━━━━━\n[⚜️] ➜ ${global.config.PREFIX}${this.config.name} add @tag hoặc phản hồi: thêm thành viên làm quản trị viên nhóm\n[⚜️] ➜ ${global.config.PREFIX}${this.config.name} remove @tag hoặc phản hồi: xóa quản trị viên của người khác`, event.threadID, event.messageID);
-  if (!dataThread.adminIDs.some(item => item.id == api.getCurrentUserID()) && !dataThread.adminIDs.some(item => item.id == event.senderID)) return api.sendMessage('[⚜️] ➜ Lệnh này dành cho quản trị viên nhóm!', event.threadID, event.messageID);
-  if (args[0] == 'add') {
-    if (event.type == "message_reply") {
-      var uid1 = event.senderID
-      var uid = event.messageReply.senderID
-      api.sendMessage('[⚜️] ➜ Thả cảm xúc "❤" vào tin nhắn này để xác nhận!',  event.threadID, (error, info) => {
-  global.client.handleReaction.push({
-      name: this.config.name, 
-      type: 'add' ,
-      messageID: info.messageID,
-      author: uid1,
-      userID: uid
-    })
-        event.messageID
-})
-    } else if(args.join().indexOf('@') !== -1){
-      var uid = Object.keys(event.mentions)[0]
-      var uid1 = event.senderID
-      api.sendMessage('[⚜️] ➜ Thả cảm xúc "❤" vào tin nhắn này để xác nhận!',  event.threadID, (error, info) => {
-  global.client.handleReaction.push({
-      name: this.config.name,
-      type: 'add' ,
-      messageID: info.messageID,
-      author: uid1,
-      userID: uid
-    })
-        event.messageID
-})
-    } else {
-      var uid1 = event.senderID
-      api.sendMessage('[⚜️] ➜ Thả cảm xúc "❤" vào tin nhắn này để xác nhận!',  event.threadID, (error, info) => {
-  global.client.handleReaction.push({
-      name: this.config.name,
-      type: 'add' ,
-      messageID: info.messageID,
-      author: uid1,
-      userID: uid1
-    })
-        event.messageID
-})
-    }
-  }
-    if (args[0] == 'remove') {
-    if (event.type == "message_reply") {
-      var uid1 = event.senderID
-      var uid = event.messageReply.senderID
-      api.sendMessage('[⚜️] ➜ Thả cảm xúc "❤" vào tin nhắn này để xác nhận!',  event.threadID, (error, info) => {
-  global.client.handleReaction.push({
-      name: this.config.name, 
-      type: 'remove' ,
-      messageID: info.messageID,
-      author: uid1,
-      userID: uid
-    })
-        event.messageID
-})
-    } else if(args.join().indexOf('@') !== -1){
-      var uid = Object.keys(event.mentions)[0]
-      var uid1 = event.senderID
-      api.sendMessage('[⚜️] ➜ Thả cảm xúc "❤" vào tin nhắn này để xác nhận!',  event.threadID, (error, info) => {
-  global.client.handleReaction.push({
-      name: this.config.name,
-      type: 'remove' ,
-      messageID: info.messageID,
-      author: uid1,
-      userID: uid
-    })
-        event.messageID
-})
-    }
-  }
-}
-module.exports.handleReaction = async function({ event, api, handleReaction, Currencies,Users}){
-  console.log(handleReaction)
-  if (event.userID != handleReaction.author) return;
-  if (event.reaction != "❤") return;
-  if(handleReaction.type == 'add'){
-    var name =  (await Users.getData(handleReaction.userID)).name
-          api.changeAdminStatus(event.threadID, handleReaction.userID, true, editAdminsCallback)
-          function editAdminsCallback(err) {
-            if (err) return api.sendMessage("[⚜️] ➜ Bot cần quản trị viên để sử dụng lệnh!", event.threadID, event.messageID);
-            return api.sendMessage(`[⚜️] ➜ Đã thêm: ${name} làm quản trị viên nhóm!`, event.threadID, event.messageID);
-          }
-  }
-  if(handleReaction.type == 'remove'){
-        var name =  (await Users.getData(handleReaction.userID)).name
-          api.changeAdminStatus(event.threadID, handleReaction.userID, false, editAdminsCallback)
-          function editAdminsCallback(err) {
-            if (err) return api.sendMessage("[⚜️] ➜ Bot cần quản trị viên để sử dụng lệnh!", event.threadID, event.messageID);
-            return api.sendMessage(`[⚜️] ➜ Đã gỡ bỏ quản trị viên nhóm của: ${name}`, event.threadID, event.messageID);
-          }
-  }
-  }
+module.exports.config = { 
+     "name": "qtv", 
+     "version": "1.0.0", 
+     "hasPermssion": 1, 
+     "credits": "Niiozic", 
+     "description": "Thêm hoặc xoá qtv", 
+     "commandCategory": "Nhóm", 
+     "usages": "[test]", 
+     "cooldowns": 5 
+ }; 
+ module.exports.run = async function ({ event, api, Currencies, args ,Users, Threads }) { 
+   if(!args[0]) return api.sendMessage('⚠️ Lựa chọn qtv add [tag/reply]', event.threadID) 
+   let dataThread = (await Threads.getData(event.threadID)).threadInfo; 
+   if (!dataThread.adminIDs.some(item => item.id == api.getCurrentUserID()) && !dataThread.adminIDs.some(item => item.id == event.senderID)) return api.sendMessage('❎ Bạn không đủ quyền hạn dùng lệnh này', event.threadID, event.messageID); 
+   if (args[0] == 'add') { 
+     if (event.type == "message_reply") { 
+       var uid1 = event.senderID 
+       var uid = event.messageReply.senderID 
+       api.sendMessage('📌 Thả cảm xúc tin nhắn này để xác nhận',  event.threadID, (error, info) => { 
+   global.client.handleReaction.push({ 
+       name: this.config.name,  
+       type: 'add' , 
+       messageID: info.messageID, 
+       author: uid1, 
+       userID: uid 
+     }) 
+         event.messageID 
+ }) 
+     } else if(args.join().indexOf('@') !== -1){ 
+       var uid = Object.keys(event.mentions)[0] 
+       var uid1 = event.senderID 
+       api.sendMessage('📌 Thả cảm xúc tin nhắn này để xác nhận',  event.threadID, (error, info) => { 
+   global.client.handleReaction.push({ 
+       name: this.config.name, 
+       type: 'add' , 
+       messageID: info.messageID, 
+       author: uid1, 
+       userID: uid 
+     }) 
+         event.messageID 
+ }) 
+     } else { 
+       var uid1 = event.senderID 
+       api.sendMessage('📌 Thả cảm xúc tin nhắn này để xác nhận',  event.threadID, (error, info) => { 
+   global.client.handleReaction.push({ 
+       name: this.config.name, 
+       type: 'add' , 
+       messageID: info.messageID, 
+       author: uid1, 
+       userID: uid1 
+     }) 
+         event.messageID 
+ }) 
+     } 
+   } 
+     if (args[0] == 'remove') { 
+     if (event.type == "message_reply") { 
+       var uid1 = event.senderID 
+       var uid = event.messageReply.senderID 
+       api.sendMessage('📌 Thả cảm xúc tin nhắn này để xác nhận',  event.threadID, (error, info) => { 
+   global.client.handleReaction.push({ 
+       name: this.config.name,  
+       type: 'remove' , 
+       messageID: info.messageID, 
+       author: uid1, 
+       userID: uid 
+     }) 
+         event.messageID 
+ }) 
+     } else if(args.join().indexOf('@') !== -1){ 
+       var uid = Object.keys(event.mentions)[0] 
+       var uid1 = event.senderID 
+       api.sendMessage('📌 Thả cảm xúc tin nhắn này để xác nhận',  event.threadID, (error, info) => { 
+   global.client.handleReaction.push({ 
+       name: this.config.name, 
+       type: 'remove' , 
+       messageID: info.messageID, 
+       author: uid1, 
+       userID: uid 
+     }) 
+         event.messageID 
+ }) 
+     } 
+   } 
+ } 
+ module.exports.handleReaction = async function({ event, api, handleReaction, Currencies,Users}){ 
+   console.log(handleReaction) 
+   if (event.userID != handleReaction.author) return; 
+   if(handleReaction.type == 'add'){ 
+     var name =  (await Users.getData(handleReaction.userID)).name 
+           api.changeAdminStatus(event.threadID, handleReaction.userID, true, editAdminsCallback) 
+           function editAdminsCallback(err) { 
+             if (err) return api.sendMessage("❎ Bot không đủ quyền hạn để thêm quản trị viên", event.threadID, event.messageID); 
+             return api.sendMessage(`✅ Đã thêm ${name} làm qtv nhóm`, event.threadID, event.messageID); 
+           } 
+   } 
+   if(handleReaction.type == 'remove'){ 
+         var name =  (await Users.getData(handleReaction.userID)).name 
+           api.changeAdminStatus(event.threadID, handleReaction.userID, false, editAdminsCallback) 
+           function editAdminsCallback(err) { 
+             if (err) return api.sendMessage("❎ Bot không đủ quyền hạn để thêm quản trị viên", event.threadID, event.messageID); 
+             return api.sendMessage(`✅ Đã gỡ qtv của ${name}`, event.threadID, event.messageID); 
+           } 
+   } 
+     }
